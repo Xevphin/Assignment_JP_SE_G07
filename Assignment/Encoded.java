7819bf2a533eba31e2cb8a2e6eb6203361a4ad39
qhrnnisa <106089@siswa.unimas.my>
2026-05-11 16:37:03 +0800

    test

--- Assignment/Encoded.java
+++ Assignment/Encoded.java
@@ -3,6 +3,6 @@ package Assignment;
 public class Encoded {
     //test test hashimi
     public static void main (String[] args){
-        System.out.println("Hello");
+        System.out.println("hohoho");
 }
 }
